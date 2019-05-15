934c7e99b956d369d62072ee3c580c39ec8660c2
t5ip <pikaviesti.testitili@gmail.com>
2019-05-16 01:52:30 +0300

    comments

--- run.sh
+++ run.sh
@@ -8,7 +8,10 @@ source virtenv/bin/activate
 
 for filename in $WIFISCAN_FOLDER/scan-[0-9]*.csv; do
     python wlandb.py $filename
-    rm $filename
+    #todo: handle removing of old files. Keep only the 
+    #newest file which is the one that airmon-ng is 
+    #currently updating.
+    #rm $filename
 done
 
 deactivate #virtualenv
